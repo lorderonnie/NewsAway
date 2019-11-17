db4e0ecd5b9966a21af5a9024488ca4eb5893039
lorderonnie <rontheking45@gmail.com>
2019-11-17 22:22:04 +0300

    added the apps info into the views.py adding news route.

--- app/views.py
+++ app/views.py
@@ -3,6 +3,12 @@ from app import app
 
 @app.route('/')
 def index():
-    
+       
     
     return render_template('index.html')
+
+
+@app.route('/news/news_id')
+def news(news_id):
+    
+    return render_template('news.html',id = news_id)
