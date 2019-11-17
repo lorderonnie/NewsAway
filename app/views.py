367a1360505a2318b70872cf8ba4fb04b0b448e8
lorderonnie <rontheking45@gmail.com>
2019-11-18 00:52:12 +0300

    created the sources inmy views.py

--- app/views.py
+++ app/views.py
@@ -5,10 +5,11 @@ from app import app
 @app.route('/')
 def index():
     latest_news = get_news('latest')
-    
-            
+    political_news = get_news('political')
+    sport_news =   get_news('sport')
+    social_news =  get_news('social')   
     title = 'NewsAway'
-    return render_template('index.html',title = title,latest = latest_news)
+    return render_template('index.html',title = title,latest = latest_news,political = political_news,sport = sport_news,social = social_news)
 
 
 @app.route('/news/<int:news_id>')
